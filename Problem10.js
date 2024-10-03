b6c4ede3b4eb0fb89e4ca433941bfdb58ac310bd
JoshwynP <joshwynparekh@gmail.com>
2024-10-02 23:23:49 -0400

    Works but slow

--- Problem10.js
+++ Problem10.js
@@ -4,22 +4,24 @@
 *    @author Joshwyn Parekh
 */
 
-var sum  = 2;
-var prime = 3
+var sum  = 5;
+var prime = 5;
+var isPrime;
 
 while (prime < 2000000) {
+    isPrime = true;
     if (prime%3 == 0) {prime +=2;}
     else {
-        for (let i = 3; i < prime; i++) {
+        for (let i = 4; i < prime; i++) {
             if (prime%i == 0) {
                 prime+=2;
+                isPrime = false;
                 break;
             }
         }
+        if (isPrime) {sum += prime;}
+        prime+=2;
     }
 }
-
-
-
 
 console.log(sum);
